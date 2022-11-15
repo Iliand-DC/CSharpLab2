fa25b1b88af67fc6f0eff2969d492e2900014897
Илья Дудкин <iluha805@gmail.com>
2022-11-15 10:59:38 +0300

    Добавьте файлы проекта.

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveResultAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.option3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.taskToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(756, 27);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.saveResultAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(43, 23);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(206, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(206, 24);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(206, 24);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // saveResultAsToolStripMenuItem
+            // 
+            this.saveResultAsToolStripMenuItem.Name = "saveResultAsToolStripMenuItem";
+            this.saveResultAsToolStripMenuItem.Size = new System.Drawing.Size(206, 24);
+            this.saveResultAsToolStripMenuItem.Text = "Save Result As";
+            this.saveResultAsToolStripMenuItem.Click += new System.EventHandler(this.saveResultAsToolStripMenuItem_Click);
+            // 
+            // taskToolStripMenuItem
+            // 
+            this.taskToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.option1ToolStripMenuItem,
+            this.option2ToolStripMenuItem,
+            this.option3ToolStripMenuItem});
+            this.taskToolStripMenuItem.Name = "taskToolStripMenuItem";
+            this.taskToolStripMenuItem.Size = new System.Drawing.Size(48, 23);
+            this.taskToolStripMenuItem.Text = "Task";
+            // 
+            // option1ToolStripMenuItem
+            // 
+            this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
+            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(134, 24);
+            this.option1ToolStripMenuItem.Text = "option1";
+            this.option1ToolStripMenuItem.Click += new System.EventHandler(this.option1ToolStripMenuItem_Click);
+            // 
+            // option2ToolStripMenuItem
+            // 
+            this.option2ToolStripMenuItem.Name = "option2ToolStripMenuItem";
+            this.option2ToolStripMenuItem.Size = new System.Drawing.Size(134, 24);
+            this.option2ToolStripMenuItem.Text = "option2";
+            this.option2ToolStripMenuItem.Click += new System.EventHandler(this.option2ToolStripMenuItem_Click);
+            // 
+            // option3ToolStripMenuItem
+            // 
+            this.option3ToolStripMenuItem.Name = "option3ToolStripMenuItem";
+            this.option3ToolStripMenuItem.Size = new System.Drawing.Size(134, 24);
+            this.option3ToolStripMenuItem.Text = "option3";
+            this.option3ToolStripMenuItem.Click += new System.EventHandler(this.option3ToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(61, 23);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(390, 40);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(354, 165);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 165);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 212);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem taskToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem option1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem option2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem option3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveResultAsToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        private string to_lowercase(string s)
+        {
+            string res="";
+            for(int i = 0; i < s.Length; i++)
+                res+=to_lowercase(s[i]);
+            return res;
+        }
+        private char to_lowercase(char c)
+        {
+            if (c >= 'A' && c <= 'Z')
+            {
+                return (char)(c + 32);
+            }
+
+            return c;
+        }
+        private bool IsPalindrom(string s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+
+                if (s[i] != s[s.Length - i - 1])
+                    return false;
+            return true;
+        }
+        string fileName, saveFileName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                @"Дан текстовый файл, содержащий слова, разделенные одним или
+несколькими пробелами. Создать на его основе новый файл, состоящий
+только из тех строк первого файла, в которых имеются два слова – палиндрома (вне зависимости от регистра)."
+                );
+        }
+        private void option1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string s1, s2 = "";
+            textBox2.Clear();
+            for(int i=0;i<textBox1.Lines.Count();i++)
+            {
+                int polinomCount = 0;
+                s1 = textBox1.Lines[i];
+                string[] s_mas = s1.Split(' ');
+                foreach (string str in s_mas)
+                    if (IsPalindrom(str.ToLower()))
+                    {
+                        polinomCount++;
+                        s2 += String.Concat(str, ' ');
+                    }
+                if (polinomCount > 1)
+                {
+                    if (textBox2.Text.Length > 0)
+                        textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText(s2);
+                }
+                s2 = "";
+                polinomCount = 0;
+            }
+        }
+        private void option2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string s1, s2 = "", res = "";
+            textBox2.Clear();
+            for(int i=0;i<textBox1.Lines.Count();i++)
+            {
+                int polinomCount = 0;
+                s1 = textBox1.Lines[i]+' ';
+                int j = 0;
+                while (j < s1.Length)
+                {
+                    if (s1[j] != ' ')
+                    {
+                        res += s1[j];
+                    }
+                    else
+                    {
+                        if (IsPalindrom(to_lowercase(res)))
+                        {
+                            polinomCount++;
+                            s2 += res + ' ';
+                        }
+                        res = "";
+                    }
+                    j++;
+                }
+                if (polinomCount > 1)
+                {
+                    if (textBox2.Text.Length > 0)
+                        textBox2.AppendText(Environment.NewLine);
+                    textBox2.AppendText(s2);
+                }
+                s2 = "";
+            }
+        }
+        private void option3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength != 0)
+            {
+                int count;
+                byte[] byteArray;
+                char[] charArray;
+                UnicodeEncoding uniEncoding = new UnicodeEncoding();
+                byte[] secondString =
+                uniEncoding.GetBytes(Path.GetInvalidPathChars());
+                using (MemoryStream memStream =
+                new MemoryStream(textBox1.TextLength *
+                UnicodeEncoding.CharSize))
+                {
+                    memStream.Write(uniEncoding.GetBytes(textBox1.Text),
+                    0, textBox1.TextLength * UnicodeEncoding.CharSize);
+                    textBox2.Clear();
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    StreamReader memrdr = new StreamReader(memStream,
+                    Encoding.Unicode);
+                    int c1 = -1, c2 = memrdr.Read();
+                    int i = 0;
+                    while (c2 != -1)
+                    {
+                        int countWord = 0;
+                        // CR LF: возврат каретки + перевод строки,
+                        // символы Юникода 000D + 000A
+                        while (c2 != '\r' && c2 != -1)
+                        {
+                            c1 = c2;
+                            c2 = memrdr.Read();
+                            textBox2.AppendText(((char)c1).ToString());
+                        }
+                        if (c2 == '\r')
+                        {
+                            c2 = memrdr.Read();
+                            c1 = c2;
+                            c2 = memrdr.Read();
+                        }
+                        textBox2.AppendText("\n");
+                    }
+                }
+            }
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() 
+                == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader f_In = new StreamReader(openFileDialog1.FileName);
+                fileName = openFileDialog1.FileName;
+                textBox1.Text = f_In.ReadToEnd();
+                textBox2.Clear();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Текстовый документ(*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if(saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog1.FileName);
+                streamWriter.WriteLine(textBox1.Text);
+                streamWriter.Close();
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveToolStripMenuItem_Click(sender, e);
+        }
+
+        private void saveResultAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog2.Filter = "Текстовый документ(*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog2.FileName);
+                streamWriter.WriteLine(textBox2.Text);
+                streamWriter.Close();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog1.FileName);
+                streamWriter.WriteLine(textBox1.Text);
+                streamWriter.Close();
+            }
+            catch(System.ArgumentException)
+            {
+                saveAsToolStripMenuItem_Click(sender, e);
+            }
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog2.FileName);
+                streamWriter.WriteLine(textBox2.Text);
+                streamWriter.Close();
+            }
+            catch(System.ArgumentException)
+            {
+                saveResultAsToolStripMenuItem_Click(sender, e);
+            }
+        }
+    }
+}
